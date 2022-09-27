0ed5166297336e916663496dfbba7a7d4c994a78
nick <nickadaniya@gmail.com>
2022-09-27 10:35:18 -1000

    convert to base 16

--- 1-base-conversions/3-convertToBase16.js
+++ 1-base-conversions/3-convertToBase16.js
@@ -3,7 +3,11 @@
 /******************************************************************************/
 
 const convertToBase16 = element => {
-  // Your code here
+  if (typeof element === 'string') {
+    return '0x' + parseInt(element.slice(2), 2).toString(16);
+  } else {
+    return '0x' + element.toString(16);
+  }
 };
 
 /******************************************************************************/
